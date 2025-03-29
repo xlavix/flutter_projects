45d60469d0ad22153d71eaac5688f4512b50518a
Adi Wahyu Pribadi <adi.wahyu.p@gmail.com>
2025-03-29 10:39:42 +0700

    dokumentasi aplikasi

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
 import 'pages/halaman_utama.dart';
 import 'pages/halaman_hari_libur.dart';
 import 'pages/halaman_profile.dart';
